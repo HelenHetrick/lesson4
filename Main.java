b2bbc379e8815031e29bf7f827845d794b650263
HelenHetrick <94848906+HelenHetrick@users.noreply.github.com>
2022-06-13 13:15:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    for(inti=0;i<lis.size();i++)
+    {
+      if(list.get(i)%2==1)
+      {
+        System.out.println(list.get(i)+" is odd.");
+      }
+      else
+      {
+                System.out.println(list.get(i)+" is even.");
+
+      }
+    }
+  }
+}
